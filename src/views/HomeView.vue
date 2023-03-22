7eaa835aa143c9292409a359abe85129b7e7fbde
zhongyi.li <1454598684@qq.com>
2023-03-22 08:48:47 +0800

    feat: 引入sass模块

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,13 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <main>
-  </main>
+  <main class="home-container">home</main>
 </template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+  .home-container {
+    height: 100vw;
+    overflow-x: hidden;
+    overflow-y: auto;
+  }
+</style>
