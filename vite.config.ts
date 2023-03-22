e79d43f9a36e9a55b8f5fdfce4417221a7920128
zhongyi.li <1454598684@qq.com>
2023-03-22 13:34:53 +0800

    feat: 获取数据修改为jsonplaceholder方式

--- vite.config.ts
+++ vite.config.ts
@@ -33,10 +33,15 @@ export default defineConfig(({command, mode}) => {
         //   changeOrigin: true,
         //   rewrite: (path) => path.replace(/^\/api/, '')
         // }
+        // '/api': {
+        //   target: 'http://localhost:4567',
+        //   changeOrigin: true,
+        //   // rewrite: (path) => path.replace(/^\/api/, '')
+        // }
         '/api': {
-          target: 'http://localhost:4567',
+          target: 'http://jsonplaceholder.typicode.com',
           changeOrigin: true,
-          // rewrite: (path) => path.replace(/^\/api/, '')
+          rewrite: (path) => path.replace(/^\/api/, '')
         }
       }
     }
